2127d56759eeceeb94e244a31894cd18ad4739ae
Alexander Peal <aapeal04@gmail.com>
2024-11-07 13:41:08 -0500

    feat: Added api routes for CRUD user projects, XP, and ticket states

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,4 +11,13 @@ router.put('/users/:github_username', userController.updateUser);
 // Get a user XP by github username
 router.get('/users/:github_username/xp', userController.getUserXP);
 
+// Initialize user projects
+router.post('/users/:github_username/init-projects', userController.initializeUserProjects);
+
+// Update ticket status
+router.put('/users/:github_username/tickets', userController.updateTicketStatus);
+
+// Get user projects
+router.get('/users/:github_username/projects', userController.getUserProjects);
+
 module.exports = router;
