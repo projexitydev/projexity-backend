a986b147edf4adf383af580f38e5aac07322aa9a
avi22vt <157037574+avi22vt@users.noreply.github.com>
2024-11-07 20:33:21 -0500

    cors again

--- app.mjs
+++ app.mjs
@@ -37,7 +37,7 @@ app.use(passport.session());
 
 // Place this before your routes
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "http://localhost:3000/", "https://projexity.dev", "https://projexity.dev/"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
